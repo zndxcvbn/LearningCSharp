70d074b282a9a5ea62a3ef6750a7033c93441e79
zxcvbn <66063766+zndxcvbn@users.noreply.github.com>
2022-10-12 12:48:38 +0400

    Updated

---
+++ Lesson5/Lesson5/Abonent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    internal class Abonent
+    {
+        class Singleton
+        {
+            private static Singleton instance;
+
+            private Singleton()
+            { }
+
+            public static Singleton getInstance()
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        private int id;
+        private string name;
+        private long number;
+        private string country;
+
+        private Id()
+        {
+            get {
+                return id;
+            }
+            set { }
+        }
+        public Abonent(int id, string name, long number, string country)
+        {
+            this.id = id;
+            this.name = name;
+            this.number = number;
+            this.country = country;
+        }
+    }
+}
---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Metrics;
+using System.Xml.Linq;
+
+namespace Lesson5
+{
+    internal class Program
+    {
+        private static List<Abonent> dataBase = new List<Abonent>();
+
+        private static void DeleteNumber(int id)
+        {
+            dataBase.RemoveAt(id);
+        }
+
+        private static void CreateNumber( int id, string name, long number, string country )
+        {
+            dataBase.Add(new Abonent(id, name, number, country));
+        }
+
+        private static void WriteToFile()
+        {
+            string path = @"C:\Users\artem\Desktop\PhoneNumber_Database.txt";
+
+            using (StreamWriter stream = new StreamWriter(path, true))
+            {
+                stream.WriteLine("asdasd");
+            }
+        }
+
+        private static void ShowDatabase()
+        {
+            foreach (Abonent el in dataBase)
+            {
+                Console.WriteLine(el.id + "\t" + el.Year + "\t" + el.Sex + "\t" + el.YearsOld);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("База данных номеров телефонов");
+            CreateNumber(1, "Artem Medvedev", 79195046481, "Russian");
+            ShowDatabase();
+        }
+    }
+}
